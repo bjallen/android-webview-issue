a5f0b9093ce2b6d5dfe737e061bf64354361958e
B.J. Allen <ballen@numerex.com>
2016-02-19 17:35:13 -0500

    add files

---
+++ app/src/main/java/com/numerex/localweb/MainActivity.java
@@ -0,0 +1,33 @@
+package com.numerex.localweb;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+
+    private class LocalWebClient extends WebViewClient {
+        public Context context = null;
+        public LocalWebClient(Context context) {
+            this.context = context;
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Log.d("d", "finished");
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.setWebViewClient(new LocalWebClient(this));
+//        webView.loadDataWithBaseURL("/", "<body style='background-color: blue;'></body>", "text/html", "UTF-8", "");
+        webView.loadUrl("https://gotrack.com");
+    }
+}
